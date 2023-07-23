069f0ec9e111d8d0ad2c8d2e17bb6bc029dd1d53
DASXakaZORQY <140264323+DASXakaZORQY@users.noreply.github.com>
2023-07-23 16:52:20 -0700

    Add files via upload

---
+++ The_mechanism.py
@@ -0,0 +1,281 @@
+
+#asking for the numbers
+number_question1 = int(input("Enter number here: "))
+number_question2 = int(input("Enter number here: "))
+number_question3 = int(input("Enter number here: "))
+number_question4 = int(input("Enter number here: "))
+number_question5 = int(input("Enter number here: "))
+
+
+#Doing the mechanism(im too lazy to do every line)
+def Mechanism():
+    if number_question1 == 1:
+        print("a")
+    elif number_question1 == 2:
+        print("b")
+    elif number_question1 == 3:
+        print("c")
+    elif number_question1 == 4:
+        print("d")
+    elif number_question1 == 5:
+        print("e")
+    elif number_question1 == 6:
+        print("f")
+    elif number_question1 == 7:
+        print('g')
+    elif number_question1 == 8:
+        print("h")
+    elif number_question1 == 9:
+        print("i")
+    elif number_question1 == 10:
+        print("j")
+    elif number_question1 == 11:
+        print("k")
+    elif number_question1 == 12:
+        print("l")
+    elif number_question1 == 13:
+        print("m")
+    elif number_question1 == 14:
+        print("n")
+    elif number_question1 == 15:
+        print("o")
+    elif number_question1 == 16:
+        print("p")
+    elif number_question1 == 17:
+        print("q")
+    elif number_question1 == 18:
+        print("r")
+    elif number_question1 == 19:
+        print("s")
+    elif number_question1 == 20:
+        print("t")
+    elif number_question1 == 21:
+        print("u")
+    elif number_question1 == 22:
+        print("v")
+    elif number_question1 == 23:
+        print("w")
+    elif number_question1 == 24:
+        print("x")
+    elif number_question1 == 25:
+        print("y")
+    elif number_question1 == 26:
+        print("z")
+    elif number_question1 > 26:
+        print("ERROR")
+    if number_question2 == 1:
+        print("a")
+    elif number_question2 == 2:
+        print("b")
+    elif number_question2 == 3:
+        print("c")
+    elif number_question2 == 4:
+        print("d")
+    elif number_question2 == 5:
+        print("e")
+    elif number_question2 == 6:
+        print("f")
+    elif number_question2 == 7:
+        print('g')
+    elif number_question2 == 8:
+        print("h")
+    elif number_question2 == 9:
+        print("i")
+    elif number_question2 == 10:
+        print("j")
+    elif number_question2 == 11:
+        print("k")
+    elif number_question2 == 12:
+        print("l")
+    elif number_question2 == 13:
+        print("m")
+    elif number_question2 == 14:
+        print("n")
+    elif number_question2 == 15:
+        print("o")
+    elif number_question2 == 16:
+        print("p")
+    elif number_question2 == 17:
+        print("q")
+    elif number_question2 == 18:
+        print("r")
+    elif number_question2 == 19:
+        print("s")
+    elif number_question2 == 20:
+        print("t")
+    elif number_question2 == 21:
+        print("u")
+    elif number_question2 == 22:
+        print("v")
+    elif number_question2 == 23:
+        print("w")
+    elif number_question2 == 24:
+        print("x")
+    elif number_question2 == 25:
+        print("y")
+    elif number_question2 == 26:
+        print("z")
+    elif number_question2 > 26:
+        print("ERROR")
+    if number_question3 == 1:
+        print("a")
+    elif number_question3 == 2:
+        print("b")
+    elif number_question3 == 3:
+        print("c")
+    elif number_question3 == 4:
+        print("d")
+    elif number_question3 == 5:
+        print("e")
+    elif number_question3 == 6:
+        print("f")
+    elif number_question3 == 7:
+        print('g')
+    elif number_question3 == 8:
+        print("h")
+    elif number_question3 == 9:
+        print("i")
+    elif number_question3 == 10:
+        print("j")
+    elif number_question3 == 11:
+        print("k")
+    elif number_question3 == 12:
+        print("l")
+    elif number_question3 == 13:
+        print("m")
+    elif number_question3 == 14:
+        print("n")
+    elif number_question3 == 15:
+        print("o")
+    elif number_question3 == 16:
+        print("p")
+    elif number_question3 == 17:
+        print("q")
+    elif number_question3 == 18:
+        print("r")
+    elif number_question3 == 19:
+        print("s")
+    elif number_question3 == 20:
+        print("t")
+    elif number_question3 == 21:
+        print("u")
+    elif number_question3 == 22:
+        print("v")
+    elif number_question3 == 23:
+        print("w")
+    elif number_question3 == 24:
+        print("x")
+    elif number_question3 == 25:
+        print("y")
+    elif number_question3 == 26:
+        print("z")
+    elif number_question3 > 26:
+        print("ERROR")
+    if number_question4 == 1:
+        print("a")
+    elif number_question4 == 2:
+        print("b")
+    elif number_question4 == 3:
+        print("c")
+    elif number_question4 == 4:
+        print("d")
+    elif number_question4 == 5:
+        print("e")
+    elif number_question4 == 6:
+        print("f")
+    elif number_question4 == 7:
+        print('g')
+    elif number_question4 == 8:
+        print("h")
+    elif number_question4 == 9:
+        print("i")
+    elif number_question4 == 10:
+        print("j")
+    elif number_question4 == 11:
+        print("k")
+    elif number_question4 == 12:
+        print("l")
+    elif number_question4 == 13:
+        print("m")
+    elif number_question4 == 14:
+        print("n")
+    elif number_question4 == 15:
+        print("o")
+    elif number_question4 == 16:
+        print("p")
+    elif number_question4 == 17:
+        print("q")
+    elif number_question4 == 18:
+        print("r")
+    elif number_question4 == 19:
+        print("s")
+    elif number_question4 == 20:
+        print("t")
+    elif number_question4 == 21:
+        print("u")
+    elif number_question4 == 22:
+        print("v")
+    elif number_question4 == 23:
+        print("w")
+    elif number_question4 == 24:
+        print("x")
+    elif number_question4 == 25:
+        print("y")
+    elif number_question4 == 26:
+        print("z")
+    elif number_question4 > 26:
+        print("ERROR")
+    if number_question5 == 1:
+        print("a")
+    elif number_question5 == 2:
+        print("b")
+    elif number_question5 == 3:
+        print("c")
+    elif number_question5 == 4:
+        print("d")
+    elif number_question5 == 5:
+        print("e")
+    elif number_question5 == 6:
+        print("f")
+    elif number_question5 == 7:
+        print('g')
+    elif number_question5 == 8:
+        print("h")
+    elif number_question5 == 9:
+        print("i")
+    elif number_question5 == 10:
+        print("j")
+    elif number_question5 == 11:
+        print("k")
+    elif number_question5 == 12:
+        print("l")
+    elif number_question5 == 13:
+        print("m")
+    elif number_question5 == 14:
+        print("n")
+    elif number_question5 == 15:
+        print("o")
+    elif number_question5 == 16:
+        print("p")
+    elif number_question5 == 17:
+        print("q")
+    elif number_question5 == 18:
+        print("r")
+    elif number_question5 == 19:
+        print("s")
+    elif number_question5 == 20:
+        print("t")
+    elif number_question5 == 21:
+        print("u")
+    elif number_question5 == 22:
+        print("v")
+    elif number_question5 == 23:
+        print("w")
+    elif number_question5 == 24:
+        print("x")
+    elif number_question5 == 25:
+        print("y")
+    elif number_question5 == 26:
+        print("z")
+    elif number_question5 > 26:
+        print("ERROR")
---
+++ main.py
@@ -0,0 +1,13 @@
+#importing the time module
+import time
+#importing The mechanism
+import The_mechanism as TM
+
+#me printing a description
+print("This is a simple algorithm project that i made to take numbers you put in and make them into letters in an alphabetic order")
+
+
+run = True
+if run == True:
+    TM.Mechanism()
+    quit()
